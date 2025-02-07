d9efef1c66316bbd4b473b1e9c2fd515c8f08a38
dennisquispe72 <dennisquispe72@gmail.com>
2025-02-07 13:54:47 -0500

    update

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Ruta básica para probar
-Route::get('/ping', function () {
-    return response()->json(['message' => 'API funcionando correctamente']);
-});
 
-// Ruta protegida por Sanctum (si estás usando autenticación)
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('register', [RegisterController::class, 'store']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::post('me', [AuthController::class, 'me']);
