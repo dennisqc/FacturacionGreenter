791ad5840dc98e96f8ac91e6701f954ef942b6b3
dennisquispe72 <dennisquispe72@gmail.com>
2025-02-08 13:19:31 -0500

    crud finalizado

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
+use App\Rules\UniqueRucRule;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -14,7 +15,9 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        return JWTAuth::user();
+        $companies = Company::where('user_id', JWTAuth::user()->id)->get();
+
+        return response()->json($companies, 200);
     }
 
     /**
@@ -28,7 +31,8 @@ class CompanyController extends Controller
                 'required',
                 'string',
                 'regex:/^(10|20)\d{9}$/',
-                'unique:companies,ruc'
+                // 'unique:companies,ruc',
+                new \App\rules\UniqueRucRule(JWTAuth::user()->id)
             ],
             'direccion' => 'required|string',
             'logo_path' => 'nullable|image',
@@ -60,24 +64,84 @@ class CompanyController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Company $company)
+    public function show($company)
     {
-        //
+
+        $company = Company::where('ruc', $company)
+            ->where('user_id', JWTAuth::user()->id)
+            ->firstorFail();
+
+
+        return response()->json(
+            $company,
+            200
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Company $company)
+    public function update(Request $request,  $company)
     {
-        //
+
+
+        $company = Company::where('ruc', $company)
+            ->where('user_id', JWTAuth::user()->id)
+
+            ->firstorFail();
+
+
+        $data = $request->validate([
+            'razon_social' => 'required|string',
+            'ruc' => [
+                'nullable',
+                'string',
+                'regex:/^(10|20)\d{9}$/',
+                // 'unique:companies,ruc',
+                 new \App\rules\UniqueRucRule($company->id)
+            ],
+            'direccion' => 'nullable|string|min:5',
+            'logo_path' => 'nullable|image',
+            'sol_user' => 'nullable|string|min:5',
+            'sol_pass' => 'nullable|string|min:5',
+            'cert_path' => 'nullable|file|mimes:pem,txt',
+            'client_id' => 'nullable|string',
+            'client_secret' => 'nullable|string',
+            'production' => 'nullable|boolean',
+        ]);
+
+        if ($request->hasFile('logo_path')) {
+            $data['logo_path'] =  $request->file('logo_path')->store('logo');;
+        }
+        if ($request->hasFile('cert_path')) {
+            $data['cert_path'] = $request->file('cert_path')->store('cert_paths');
+        }
+
+        $company->update($data);
+
+        return response()->json([
+            'message' => 'Empresa Actualizada',
+            'company' => $company
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Company $company)
+    public function destroy( $company)
     {
-        //
+        $company = Company::where('ruc', $company)
+        ->where('user_id', JWTAuth::user()->id)
+
+        ->firstorFail();
+
+
+       $company->delete();
+
+       return response()->json([
+        'message' => 'Empresa eliminada',
+        'company' => $company
+    ], 200);
+
     }
 }
--- app/Rules/UniqueRucRule.php
+++ app/Rules/UniqueRucRule.php
@@ -2,11 +2,19 @@
 
 namespace App\Rules;
 
+use App\Models\Company;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UniqueRucRule implements ValidationRule
 {
+
+    public $company_id;
+    public function __construct($company_id = null)
+    {
+        $this->company_id = $company_id;
+    }
     /**
      * Run the validation rule.
      *
@@ -14,6 +22,14 @@ class UniqueRucRule implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        $company = Company::where('ruc', $value)
+            ->where('user_id', JWTAuth::user()->id)
+            ->when($this->company_id, function ($query, $company_id) {
+                $query->where('id', '!=', $company_id);
+            })
+            ->first();
+        if ($company) {
+            $fail("La compañia ya existe");
+        }
     }
 }
